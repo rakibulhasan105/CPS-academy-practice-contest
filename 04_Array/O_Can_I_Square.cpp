725c589293dbedf410276bb29c7eac0f2bb479b0
Rakibul Hasan <rakibulhasan105105@gmail.com>
2026-09-22 13:02:15 +0600

    Add O_Can_I_Square.cpp for square sum check

---
+++ 04_Array/O_Can_I_Square.cpp
@@ -0,0 +1,39 @@
+/*  আমার কাছে তোমার লেখা কোন চিঠি নেই, কোন প্রতিস্রতি নেই..
+ *  সাদায় কিন্তু অদ্ভুত তুমি..
+ */
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+#define fastio   cin.tie(0); ios_base ::sync_with_stdio(0);
+#define ll long long
+
+void phobia(int Shuhashini)
+{
+    int n;
+    cin >> n;
+    ll sum = 0;
+    for(int i = 1; i <= n;i++)
+    {
+        ll temp ; 
+        cin >> temp;
+        sum+=temp;
+    }
+    
+    ll p_squere = sqrt(sum);
+    if(p_squere*p_squere ==sum) cout << "Yes" << endl;
+    else cout << "No" << endl;
+   return; 
+}
+
+int32_t main()
+{
+    fastio;
+    int tc = 1;
+    cin >> tc;
+    while (tc--)
+    {
+        phobia(tc);
+    }
+
+    return 0; //                Hey, it's like a phobia..
+}
